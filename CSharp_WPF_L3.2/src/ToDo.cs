7328a0a2b523c62b9b87a62a428264b27f02a2b2
user <user@user.ru>
2024-06-24 21:16:15 +0300

    Добавьте файлы проекта.

---
+++ CSharp_WPF_L3.2/src/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_WPF_L3._2.src
+{
+    public class ToDo
+    {
+        private string Title {  get; set; }
+        private DateTime Date { get; set; }
+        private string Description { get; set; }
+        public ToDo() { }
+
+        public ToDo(string title, DateTime date, string description) 
+        {
+            this.Title = title;
+            this.Date = date;
+            this.Description = description;
+        }
+    }
+}
